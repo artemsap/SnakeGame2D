49df99bd540c8be29001008bd16462a2e7a4ac2c
@artemsap <a.sapozhnikov00@gmail.com>
2025-09-19 19:54:33 +0300

    Random position of apple in level

--- SnakeGame2D/GameApple.cpp
+++ SnakeGame2D/GameApple.cpp
@@ -1,9 +1,12 @@
 #include "GameApple.h"
 #include "GameSnake.h"
 
+#include <cstdlib>
+
 GameApple::GameApple(sf::Vector2f tileSize_, sf::Vector2u gridSize_) :
 	tileSize(tileSize_), shape(std::min(tileSize.x, tileSize.y) / 3), gridSize(gridSize_)
 {
+	srand(static_cast<unsigned>(time(0)));
 	shape.setPosition(-tileSize);
 }
 
@@ -25,6 +28,7 @@ const sf::Vector2i& GameApple::GetPositionOnGrid() const
 
 sf::Vector2i GameApple::GeneratePositionOnGrid(const GameSnake& snake)
 {
-	//generation here
-	return { 1, 1 };
+	int randPosX = rand() % gridSize.x;
+	int randPosY = rand() % gridSize.y;
+	return { randPosX , randPosY };
 }
