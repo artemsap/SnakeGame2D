69d831b671b9dea489fee69fa6c26c4e81b38090
@artemsap <a.sapozhnikov00@gmail.com>
2025-09-30 10:38:10 +0300

    syntax sugar

--- SnakeGame2D/main.cpp
+++ SnakeGame2D/main.cpp
@@ -4,6 +4,6 @@
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR    lpCmdLine,	_In_ int       nCmdShow)
 {
-	GameApp app({ 1000, 1000, "SFML works!" }, GameLevel::LevelSettings{ .width = 10, .height = 10, .snakeSpeed = 4 });
+	GameApp app({.width = 1000, .height = 1000, .title = "SFML works!" }, { .width = 10, .height = 10, .snakeSpeed = 4 });
 	app.Run();
 }
