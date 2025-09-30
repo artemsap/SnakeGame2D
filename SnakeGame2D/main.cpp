2027432d9ad1763ec63dfec2e697d52dafdab077
@artemsap <a.sapozhnikov00@gmail.com>
2025-09-30 10:42:06 +0300

    fix indentation

--- SnakeGame2D/main.cpp
+++ SnakeGame2D/main.cpp
@@ -2,7 +2,7 @@
 
 #include <windows.h>
 
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR    lpCmdLine,	_In_ int       nCmdShow)
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
 {
 	GameApp app({.width = 1000, .height = 1000, .title = "SFML works!" }, { .width = 10, .height = 10, .snakeSpeed = 4 });
 	app.Run();
