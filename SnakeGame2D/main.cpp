e516135d406281c64d9eaf2c05912f24e3fdc078
@artemsap <a.sapozhnikov00@gmail.com>
2025-09-30 10:35:47 +0300

    Turn off console, change file hierarchy

--- SnakeGame2D/main.cpp
+++ SnakeGame2D/main.cpp
@@ -1,7 +1,9 @@
 #include "GameApp.h"
 
-int main()
+#include <windows.h>
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR    lpCmdLine,	_In_ int       nCmdShow)
 {
-    GameApp app({ 1000, 1000, "SFML works!" }, {10, 10, 4});
-    app.Run();
+	GameApp app({ 1000, 1000, "SFML works!" }, GameLevel::LevelSettings{ .width = 10, .height = 10, .snakeSpeed = 4 });
+	app.Run();
 }
